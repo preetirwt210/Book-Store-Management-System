c91c3a82b2867edef25036e759028cc280bcf567
Preeti Rawat <preetirawat711@gmail.com>
2020-12-21 17:28:51 +0530

    Category Module Management

--- src/com/bookstore/dao/CategoryDAO.java
+++ src/com/bookstore/dao/CategoryDAO.java
@@ -10,30 +10,30 @@ public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category
 
 	public CategoryDAO(EntityManager entityManager) {
 		super(entityManager);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public Category get(Object id) {
-		// TODO Auto-generated method stub
+	
 		return null;
 	}
 
 	@Override
 	public void delete(Object id) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public List<Category> listAll() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public long count() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
